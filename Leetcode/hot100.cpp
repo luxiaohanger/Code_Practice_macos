90dd3c3f00ebbde9fc7a430fce3ea17314df291f
luxiaohang <2830908373@qq.com>
2026-04-01 09:50:58 +0800

    4.1

--- Leetcode/hot100.cpp
+++ Leetcode/hot100.cpp
@@ -11,3 +11,30 @@ vector<int> twoSum(vector<int> &nums, int target) {
     }
     return vector<int>{};
 }
+
+//49
+vector<vector<string> > groupAnagrams(vector<string> &strs) {
+    int n = strs.size();
+    vector<string> cp(strs.begin(), strs.end());
+    vector<vector<string> > ans;
+    for (int i = 0; i < n; ++i) {
+        sort(cp[i].begin(), cp[i].end());
+    }
+    unordered_map<string, int> ump;
+    for (int i = 0; i < n; ++i) {
+        if (ump.find(cp[i]) != ump.end()) {
+            ans[ump[cp[i]]].push_back(strs[i]);
+        } else {
+            ump[cp[i]] = ans.size();
+            ans.push_back({strs[i]});
+        }
+    }
+    return ans;
+}
+
+
+int main() {
+    vector<string> in{"eat", "tea", "tan", "ate", "nat", "bat"};
+    auto ans = groupAnagrams(in);
+    return 0;
+}
